83f2b3ed03de55f090ee0424f1e9e504cdd99746
GrossBean <114438164+GrossBeanQB@users.noreply.github.com>
2025-05-30 18:16:38 +0200

    Update server.lua  Player check nil fix

--- gb-drugsnpc/server.lua
+++ gb-drugsnpc/server.lua
@@ -2,6 +2,10 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 QBCore.Functions.CreateCallback('gb-drugsnpc:server:checkDrugs', function(source, cb)
     local Player = QBCore.Functions.GetPlayer(source)
+    if not Player then
+        cb({})
+        return
+    end
     local items = {}
     for item, data in pairs(Config.DrugItems) do
         local hasItem = Player.Functions.GetItemByName(item)
@@ -14,6 +18,10 @@ end)
 
 QBCore.Functions.CreateCallback('gb-drugsnpc:server:checkSpecificDrug', function(source, cb, drug, amount)
     local Player = QBCore.Functions.GetPlayer(source)
+    if not Player then
+        cb(false)
+        return
+    end
     local hasItem = Player.Functions.GetItemByName(drug)
     if hasItem and hasItem.amount >= amount then
         Player.Functions.RemoveItem(drug, amount)
@@ -26,6 +34,7 @@ end)
 RegisterServerEvent('gb-drugsnpc:server:completeBulkSale', function(drug, amount, price)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
+    if not Player then return end
     local total = price * amount
     Player.Functions.AddMoney('cash', total)
 end)
@@ -33,6 +42,7 @@ end)
 RegisterServerEvent('gb-drugsnpc:server:recoverDrugAmount', function(drug, amount)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
+    if not Player then return end
     Player.Functions.AddItem(drug, amount)
 end)
 
@@ -41,7 +51,7 @@ RegisterServerEvent('gb-drugsnpc:server:alertPolice', function(coords)
     local Players = QBCore.Functions.GetPlayers()
     for i = 1, #Players do
         local Player = QBCore.Functions.GetPlayer(Players[i])
-        if Player.PlayerData.job.name == "police" then
+        if Player and Player.PlayerData.job.name == "police" then
             TriggerClientEvent("gb-drugsnpc:client:policeAlert", Players[i], coords)
         end
     end
